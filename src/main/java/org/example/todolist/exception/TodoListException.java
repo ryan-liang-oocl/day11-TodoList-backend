9d472ad1f261f00dc0c3dffbcd080617ace28d17
Ryan Liang <ryan.liang@oocl.com>
2024-12-03 08:37:19 +0800

    refactor

--- src/main/java/org/example/todolist/exception/TodoListException.java
+++
@@ -1,7 +0,0 @@
-package org.example.todolist.exception;
-
-public class TodoListException {
-    public static void NotFoundException() {
-        throw new RuntimeException("This Todo not existed");
-    }
-}
--- src/main/java/org/example/todolist/service/TodoListService.java
+++ src/main/java/org/example/todolist/service/TodoListService.java
@@ -1,7 +1,7 @@
 package org.example.todolist.service;
 
 import org.example.todolist.Repo.TodoListRepository;
-import org.example.todolist.exception.TodoListException;
+import org.example.todolist.exception.NotFoundException;
 import org.example.todolist.model.TodoItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +10,7 @@ import java.util.List;
 
 @Service
 public class TodoListService {
+    public static final String TODO_ITEM_NOT_FOUND_MSG = "Todo item not found";
     @Autowired
     private TodoListRepository todoListRepository;
     public List<TodoItem> getAll() {
@@ -23,7 +24,7 @@ public class TodoListService {
     public TodoItem update(Integer id, TodoItem todoItem) {
         TodoItem existingTodoItem = todoListRepository.findById(id).orElse(null);
         if (existingTodoItem == null) {
-            TodoListException.NotFoundException();
+            throw new NotFoundException(TODO_ITEM_NOT_FOUND_MSG);
         }
         existingTodoItem.setText(todoItem.getText());
         existingTodoItem.setDone(todoItem.getDone());
@@ -34,7 +35,7 @@ public class TodoListService {
     public TodoItem delete(Integer id) {
         TodoItem todoItem = todoListRepository.findById(id).orElse(null);
         if (todoItem == null) {
-            TodoListException.NotFoundException();
+            throw new NotFoundException(TODO_ITEM_NOT_FOUND_MSG);
         }
         todoListRepository.delete(todoItem);
         return todoItem;
@@ -43,7 +44,7 @@ public class TodoListService {
     public TodoItem get(Integer id) {
         TodoItem todoItem = todoListRepository.findById(id).orElse(null);
         if (todoItem == null) {
-            TodoListException.NotFoundException();
+            throw new NotFoundException(TODO_ITEM_NOT_FOUND_MSG);
         }
         return todoItem;
     }
